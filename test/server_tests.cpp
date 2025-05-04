d7dc1c4991c92e7c76292857d361cc8133560aa3
RustamBH <kamaji@mail.ru>
2025-05-04 22:51:31 +0400

    Update server_tests.cpp

--- test/server_tests.cpp
+++ test/server_tests.cpp
@@ -4,23 +4,15 @@
 using namespace https_server;
 
 TEST(RequestHandlerTest, HandleValidRequest) {
-	net::io_context ioc;
-	ssl::context ctx{ ssl::context::tlsv12 };
+	http::request<http::dynamic_body> request_;
 
-	// SSL контекст	
-	ctx.set_options(
-		ssl::context::default_workarounds |
-		ssl::context::no_sslv2 |
-		ssl::context::single_dh_use);
-
-	// Загружаем сертификат и приватный ключ
-	ctx.use_certificate_chain_file("server.crt");
-	ctx.use_private_key_file("server.key", ssl::context::pem);
-
-	https_client::HttpsClient client{ ioc, ctx };
-	client.request("localhost", "4433", "/index.html");
+	response_.set(http::field::server, "Boost.Asio HTTPS Server");
+	response_.set(http::field::content_type, "text/html");
+	beast::ostream(response_.body()) << "Hello https\n";
 	
-	EXPECT_TRUE(response.find("HTTP/1.1 200 OK") != std::string::npos);
+	std::string response = get_response();
+	
+	EXPECT_TRUE(response.find("HTTP/1.1 Hello https OK") != std::string::npos);
 
 }
 
